ed29270add2263703d13f76d4b5016a89d1c4bf6
jaisonrobson <xd_jrg@hotmail.com>
2024-06-13 17:32:35 -0300

    feat: feito tratamento do apelido do usuario via post & validacao do mesmo

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,14 @@
 module.exports.iniciaChat = function(application, req, res) {
+    var dadosForm = req.body;
+
+    req.assert('apelido', 'Apelido obrigatório').notEmpty();
+    req.assert('apelido', 'Apelido deve conter de 3 a 15 caracteres').len(3, 15);
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+        res.render('index', {validacao: errors});
+    }
+
     res.render('chat');
 }
