0e86126ded539b4874449a8255fbed619b778304
jaisonrobson <xd_jrg@hotmail.com>
2024-06-13 17:20:41 -0300

    chore: refatorado/configurado rotas para funcionar atraves dos controllers

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,9 +1,9 @@
 module.exports = function(application) {
     application.post('/chat', function(req, res){
-        res.send('chat');
+        application.app.controllers.chat.iniciaChat(application, req, res);
     });
 
     application.get('/chat', function(req, res){
-        res.send('chat');
+        application.app.controllers.chat.iniciaChat(application, req, res);
     });
 }
