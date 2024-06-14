56e6fd8f5b075e3d591e0e8354eac10104236757
jaisonrobson <xd_jrg@hotmail.com>
2024-06-14 15:05:03 -0300

    feat: feito funcionamento do chat com comunicacao bidirecional `websocket`

--- app.js
+++ app.js
@@ -14,4 +14,9 @@ io.on('connection', function(socket) {
     socket.on('disconnect', function() {
         console.log('Usuario desconectou');
     });
+
+    socket.on('msgParaServidor', function(data) {
+        socket.emit('msgParaCliente', { apelido: data.apelido, mensagem: data.mensagem });
+        socket.broadcast.emit('msgParaCliente', { apelido: data.apelido, mensagem: data.mensagem });
+    });
 });
